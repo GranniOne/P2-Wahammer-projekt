8272c1a967f8464273e85912b263b40301b9023b
GranniOne <128929113+GranniOne@users.noreply.github.com>
2026-02-27 01:03:30 +0100

    adding animation and keyframes to loginpage

--- src/main/java/com/P2/warhammer/views/LoginView.java
+++ src/main/java/com/P2/warhammer/views/LoginView.java
@@ -8,16 +8,25 @@ import com.P2.warhammer.utilities.ServiceProvider;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.login.AbstractLogin;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.dom.Style;
 import com.vaadin.flow.router.*;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.server.streams.DownloadHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Route("login")
 @PageTitle("login page")
@@ -32,23 +41,44 @@ public class LoginView extends Div implements BeforeEnterObserver{
 
     public LoginView(ServiceProvider services) {
         this.services = services;
+
+
+        LoginI18n i18n = LoginI18n.createDefault();
+        i18n.setForm(new LoginI18n.Form());
+        i18n.getForm().setTitle("Warhammer Portal");
+        i18n.getForm().setUsername("Email");
+        i18n.getForm().setPassword("Password");
+        i18n.getForm().setSubmit("Enter");
+        i18n.setAdditionalInformation("Warhammer is a really insane roleplaying game, so login at your own discretion");
+
+        loginForm.setI18n(i18n);
+
+        loginForm.setClassName("loginform");
+
+        setClassName("div-page");
+
+
+        Image image = new Image(DownloadHandler.forServletResource("/images/test2.jpg"), "");
+        image.addClassName("background-img");
+        add(image);
+
+        Div loginformDiv = new Div();
+        loginformDiv.setClassName("div-login");
+
         loginForm.setAction("login"); // must match Spring Security endpoint
-        getStyle()
-                .set("display", "flex")
-                .set("flex-direction", "column")   // vertical instead of horizontal
-                .set("align-items", "center")
-                .setHeight("100%")
-                .set("justify-content","center");     // center horizontally
-        add(loginForm);
+
+        loginformDiv.add(loginForm);
+
+
+
 
 
         Button RegisterButton = new Button("Sign up", new Icon(VaadinIcon.ACADEMY_CAP), buttonClickEvent -> {
             UI.getCurrent().navigate("signup");
         });
 
-        add(RegisterButton);
 
-
+        add(loginformDiv);
     }
 
     @Override
@@ -56,6 +86,7 @@ public class LoginView extends Div implements BeforeEnterObserver{
         if (beforeEnterEvent.getLocation().getQueryParameters().getParameters().containsKey("error")) {
             loginForm.setError(true); // shows error if login fails
         }
+
     }
 
 
