0ba5a3611dcce8ec01c144a76bddecb467c5fa3f
axel <@>
2026-03-13 12:05:41 +0100

    added username change validation and page reload

--- src/main/java/com/P2/warhammer/views/ProfileView.java
+++ src/main/java/com/P2/warhammer/views/ProfileView.java
@@ -1,16 +1,14 @@
 package com.P2.warhammer.views;
 
-import com.P2.warhammer.layout.Navigation;
 import com.P2.warhammer.users.User;
 import com.P2.warhammer.users.UserService;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.Layout;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.PermitAll;
@@ -24,28 +22,44 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class  ProfileView extends VerticalLayout {
 
     private final UserService userService;
+    private final Binder<User> userBinder;
 
     ProfileView(UserService userService) {
+
+        this.userBinder = new Binder<>();
 
         // Greeter
         this.userService = userService;
         Authentication auth =  SecurityContextHolder.getContext().getAuthentication();
 
+
         // Change username functionality
         TextField usernameTextfield = new TextField("New username");
+
+        userBinder.forField(usernameTextfield).withValidator(name ->
+                        userService.findFromUsername(name) == null,
+                "User already exists").bind(User::getUsername, User::setUsername);
+
         Button usernameButton = new Button("Change username", e -> {
-            User user = userService.findFromEmail(auth.getName());
-            user.setUsername(usernameTextfield.getValue());
-            userService.saveUser(user);
-            // tilføj validator, confirm og auto reload page
+
+            if(userBinder.validate().isOk()) {
+                User user = userService.findFromEmail(auth.getName());
+                user.setUsername(usernameTextfield.getValue());
+                userService.saveUser(user);
+                UI.getCurrent().getPage().reload();
+                // tilføj validator, confirm og auto reload page
+                }
         });
 
 
+
+
+        // Layout
         add(new H1("Hello " + userService.findFromEmail(auth.getName()).getUsername()));
         add(new H3("Profile settings"));
-        // Change username field
         add(usernameTextfield);
         add(usernameButton);
 
     }
+
 }
