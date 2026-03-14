e647fcc657435af5c72cd18eefdf597cf09a9e0c
Axel <axel.lindahl@siemens.dk>
2026-03-14 18:03:50 +0100

    Changed header in profile view

--- src/main/java/com/P2/warhammer/views/ProfileView.java
+++ src/main/java/com/P2/warhammer/views/ProfileView.java
@@ -52,7 +52,7 @@ public class  ProfileView extends VerticalLayout {
         layout.setHeight("auto");
         layout.setMargin(true);
 
-        H3 profileInformationHeader = new H3("Profile information");
+        H3 profileInformationHeader = new H3("User information");
         H3 profileSettingHeader = new H3("Profile settings");
 
         layout.setAlignItems(FlexComponent.Alignment.START);
