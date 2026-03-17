656fe67cc3413e4690d7c25cb9504c144615fbb2
Axel <axel.lindahl@siemens.dk>
2026-03-17 13:52:30 +0100

    Added change password functionality to profile page

---
+++ src/main/java/com/P2/warhammer/profile/ChangePasswordComponent.java
@@ -0,0 +1,69 @@
+package com.P2.warhammer.profile;
+
+import com.P2.warhammer.users.User;
+import com.P2.warhammer.users.UserService;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.validator.StringLengthValidator;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import org.springframework.security.core.Authentication;
+
+public class ChangePasswordComponent extends Div {
+
+    public ChangePasswordComponent(UserService userService, Authentication auth){
+        Binder<User> passwordBinder = new Binder<>();
+
+        PasswordField passwordField = new PasswordField("Password");
+        passwordField.setValueChangeMode(ValueChangeMode.EAGER);
+        PasswordField confirmPasswordField = new PasswordField("Confirm password");
+        confirmPasswordField.setValueChangeMode(ValueChangeMode.EAGER);
+
+        passwordBinder.forField(passwordField)
+                .withValidator(new StringLengthValidator(
+                        "Password must be between 5 and 25 characters", 5, 25))
+                .bind(User::getPassword, User::setPassword);
+
+        passwordBinder.forField(confirmPasswordField)
+                .withValidator(confirm -> confirm.equals(passwordField.getValue()),"passwords must match")
+                .bind(User::getPassword, User::setPassword);
+
+        Dialog changePasswordDialog = new Dialog();
+
+        Button confirmPasswordButton = new Button("Confirm", e -> {
+            User user = userService.findFromEmail(auth.getName());
+            user.setPassword(confirmPasswordField.getValue());
+            userService.saveUser(user);
+            UI.getCurrent().getPage().reload();
+        });
+        confirmPasswordButton.getStyle().set("margin-left", "auto");
+
+        Button cancelPasswordButton = new Button("Cancel", e -> {
+            changePasswordDialog.close();
+        });
+        cancelPasswordButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
+
+
+        Button changePasswordButton = new Button("Confirm password change", e -> {
+            // construct dialog box
+            changePasswordDialog.open();
+        });
+
+        changePasswordDialog.add("Change password?");
+        changePasswordDialog.getFooter().add(cancelPasswordButton);
+        changePasswordDialog.getFooter().add(confirmPasswordButton);
+
+        // Layout
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.add(passwordField);
+        verticalLayout.add(confirmPasswordField);
+        verticalLayout.add(changePasswordButton);
+        this.add(verticalLayout);
+
+    }
+}
--- src/main/java/com/P2/warhammer/views/ProfileView.java
+++ src/main/java/com/P2/warhammer/views/ProfileView.java
@@ -1,29 +1,17 @@
 package com.P2.warhammer.views;
 
+import com.P2.warhammer.profile.ChangePasswordComponent;
 import com.P2.warhammer.profile.UsernameChangeComponent;
-import com.P2.warhammer.users.User;
 import com.P2.warhammer.users.UserService;
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.StyleSheet;
-import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.binder.Binder;
-import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility;
 import jakarta.annotation.security.PermitAll;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.awt.*;
-import java.util.Locale;
 
 
 @Route("profile")
@@ -32,15 +20,12 @@ import java.util.Locale;
 @StyleSheet("css/profileStyle.css")
 public class  ProfileView extends VerticalLayout {
 
-    private final UserService userService;
-
     ProfileView(UserService userService) {
-        this.userService = userService;
         Authentication auth =  SecurityContextHolder.getContext().getAuthentication();
 
         // Components
         UsernameChangeComponent usernameChangeComponent = new UsernameChangeComponent(userService, auth);
-
+        ChangePasswordComponent changePasswordComponent = new ChangePasswordComponent(userService, auth);
 
         setSizeFull();
         setAlignItems(FlexComponent.Alignment.CENTER);
@@ -66,6 +51,9 @@ public class  ProfileView extends VerticalLayout {
         layout.setAlignSelf(Alignment.CENTER, profileSettingHeader);
 
         layout.add(usernameChangeComponent);
+        layout.setAlignSelf(FlexComponent.Alignment.START, usernameChangeComponent);
+
+        layout.add(changePasswordComponent);
         layout.setAlignSelf(FlexComponent.Alignment.START, usernameChangeComponent);
 
         add(layout);
