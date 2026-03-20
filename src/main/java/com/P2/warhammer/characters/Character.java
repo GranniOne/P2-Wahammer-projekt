48f6155cd7c82c809bb78884d3d934bb3c4d84e8
GranniOne <grantzaumathias@gmail.com>
2026-03-20 14:40:08 +0100

    temporary changes

--- src/main/java/com/P2/warhammer/characters/Character.java
+++ src/main/java/com/P2/warhammer/characters/Character.java
@@ -17,8 +17,6 @@ import java.util.List;
 
  */
 @Document(collection = "characters")
-@Getter
-@Setter
 public class Character {
 
     @Id
@@ -66,4 +64,179 @@ public class Character {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getAdvantage() {
+        return advantage;
+    }
+
+    public int getMaxWounds() {
+        return maxWounds;
+    }
+
+    public int getDamageTaken() {
+        return damageTaken;
+    }
+
+    public List<Integer> getArmourValues() {
+        return armourValues;
+    }
+
+    public int getCorruptionMax() {
+        return corruptionMax;
+    }
+
+    public int getCorruptionTaken() {
+        return corruptionTaken;
+    }
+
+    public int getPfennings() {
+        return Pfennings;
+    }
+
+    public int getSilverShillings() {
+        return silverShillings;
+    }
+
+    public int getGoldCrowns() {
+        return goldCrowns;
+    }
+
+    public int getStatusLevel() {
+        return statusLevel;
+    }
+
+    public List<Object> getInventory() {
+        return inventory;
+    }
+
+    public List<Object> getEquippedArmour() {
+        return equippedArmour;
+    }
+
+    public List<Object> getEquippedWeapons() {
+        return equippedWeapons;
+    }
+
+    public List<Object> getTalents() {
+        return talents;
+    }
+
+    public List<Object> getSkills() {
+        return skills;
+    }
+
+    public List<Object> getConditions() {
+        return conditions;
+    }
+
+    public List<Integer> getCharacteristics() {
+        return characteristics;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public void setAdvantage(int advantage) {
+        this.advantage = advantage;
+    }
+
+    public void setMaxWounds(int maxWounds) {
+        this.maxWounds = maxWounds;
+    }
+
+    public void setDamageTaken(int damageTaken) {
+        this.damageTaken = damageTaken;
+    }
+
+    public void setArmourValues(List<Integer> armourValues) {
+        this.armourValues = armourValues;
+    }
+
+    public void setCorruptionMax(int corruptionMax) {
+        this.corruptionMax = corruptionMax;
+    }
+
+    public void setCorruptionTaken(int corruptionTaken) {
+        this.corruptionTaken = corruptionTaken;
+    }
+
+    public void setPfennings(int pfennings) {
+        Pfennings = pfennings;
+    }
+
+    public void setSilverShillings(int silverShillings) {
+        this.silverShillings = silverShillings;
+    }
+
+    public void setGoldCrowns(int goldCrowns) {
+        this.goldCrowns = goldCrowns;
+    }
+
+    public void setStatusLevel(int statusLevel) {
+        this.statusLevel = statusLevel;
+    }
+
+    public void setInventory(List<Object> inventory) {
+        this.inventory = inventory;
+    }
+
+    public void setEquippedArmour(List<Object> equippedArmour) {
+        this.equippedArmour = equippedArmour;
+    }
+
+    public void setEquippedWeapons(List<Object> equippedWeapons) {
+        this.equippedWeapons = equippedWeapons;
+    }
+
+    public void setTalents(List<Object> talents) {
+        this.talents = talents;
+    }
+
+    public void setSkills(List<Object> skills) {
+        this.skills = skills;
+    }
+
+    public void setConditions(List<Object> conditions) {
+        this.conditions = conditions;
+    }
+
+    public void setCharacteristics(List<Integer> characteristics) {
+        this.characteristics = characteristics;
+    }
 }
