e624a62f8901294d71bf13f91895522394fbd522
GranniOne <128929113+GranniOne@users.noreply.github.com>
2026-03-26 09:24:59 +0100

    Update CharacterRepository.java

--- src/main/java/com/P2/warhammer/characters/CharacterRepository.java
+++ src/main/java/com/P2/warhammer/characters/CharacterRepository.java
@@ -16,5 +16,5 @@ import java.util.List;
 @Repository
 public interface CharacterRepository extends MongoRepository<Character, String> {
 
-    List<Character> getCharacterByUser(String user);
+    List<Character> getCharactersByUser(String user);
 }
