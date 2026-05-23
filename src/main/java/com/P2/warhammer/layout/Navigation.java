67f9eeec0ecc7878ac5f34863fa94c77bd5ff022
mathi <grantzaumathias@gmail.com>
2026-05-23 13:59:33 +0200

    changes to the Navigation class

--- src/main/java/com/P2/warhammer/layout/Navigation.java
+++ src/main/java/com/P2/warhammer/layout/Navigation.java
@@ -1,7 +1,7 @@
 package com.P2.warhammer.layout;
 
 import com.P2.warhammer.utilities.Utilities;
-import com.P2.warhammer.views.CharacterCreatorView;
+import com.P2.warhammer.views.*;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
@@ -13,6 +13,8 @@ import com.vaadin.flow.router.QueryParameters;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.spring.security.AuthenticationContext;
 import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * the Navigation layout, is automatically implemented by spring using the @Layout annotation, do not use it manually
@@ -35,27 +37,31 @@ public class Navigation extends AppLayout {
                     .set("width", "100%").set("display", "flex");
 
             // Static navigation buttons
-            Button homeButton = new Button("Home", e -> UI.getCurrent().navigate(""));
-            Button profileButton = new Button("Profile", e -> UI.getCurrent().navigate("profile"));
-            Button profileCharactersButton = new Button("View characters", e -> UI.getCurrent().navigate("profileCharacters"));
-            Button campaignViewButton = new Button("characterCreator", e -> UI.getCurrent().navigate("characterCreator"));
-            Button campaigncreator = new Button("campaigncreator", e -> UI.getCurrent().navigate("campaigncreator"));
-            Button admindashboard = new Button("admin dashboard", e -> UI.getCurrent().navigate("admin-dashboard"));
-            Button characterCreatorbutton = new Button("button", e -> UI.getCurrent().navigate(CharacterCreatorView.class, QueryParameters.of("Character","65f1a2b3c4d5e6f7a8b90123")));
+            Button homeButton = new Button("Home", e -> UI.getCurrent().navigate(DashBoard.class));
+            Button profileButton = new Button("Profile", e -> UI.getCurrent().navigate(ProfileView.class));
+            Button campaignViewButton = new Button("characterCreator", e -> UI.getCurrent().navigate(CharacterCreatorView.class));
+            Button campaigncreator = new Button("campaigncreator", e -> UI.getCurrent().navigate(CampaignCreatorView.class));
+            Button admindashboard =  new Button("admin dashboard", e -> UI.getCurrent().navigate(AdminDashboardView.class));
+
+
+
 
             logoutButton = new Button("Log out", e -> authenticationContext.logout());
 
             homeButton.setIcon(new Icon(VaadinIcon.HOME));
             profileButton.setIcon(new Icon(VaadinIcon.USER));
             campaigncreator.setIcon(new Icon(VaadinIcon.COG));
-            profileCharactersButton.setIcon(new Icon(VaadinIcon.MALE));
             logoutButton.setIcon(new Icon(VaadinIcon.SIGN_OUT));
             campaignViewButton.setIcon(new Icon(VaadinIcon.MAP_MARKER));
 
-            navBar.addToStart(homeButton, profileButton, campaigncreator, campaignViewButton, profileCharactersButton, admindashboard,characterCreatorbutton);
+            navBar.addToStart(homeButton, profileButton, campaigncreator, campaignViewButton);
+            if (authenticationContext.hasRole("ADMIN")) {
+                navBar.addToStart(admindashboard);
+            }
             navBar.addToEnd(logoutButton);
             this.addToNavbar(navBar);
 
         }
     }
+
 }
