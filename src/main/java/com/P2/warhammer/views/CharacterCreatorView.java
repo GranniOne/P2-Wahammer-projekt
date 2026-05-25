12e2a9bc88b651a2fac4a66250293d3ea2534dbf
Malnik01 <maltean05@gmail.com>
2026-05-25 11:39:05 +0200

    minor bug-fix in updateskill

--- src/main/java/com/P2/warhammer/views/CharacterCreatorView.java
+++ src/main/java/com/P2/warhammer/views/CharacterCreatorView.java
@@ -656,19 +656,16 @@ public class CharacterCreatorView extends Div implements HasUrlParameter<String>
                 break;
             }
         }
-
         if (existingSkill == null) {
             existingSkill = new Skill();
+            existingSkill.setName(skill.getName());
+            existingSkill.setCharacteristic(skill.getCharacteristic());
+            existingSkill.setStartValue(baseField.getValue());
+            existingSkill.setBonusValue(modifierField.getValue());
+            existingSkill.setPenaltyValue(penaltyField.getValue());
+            existingSkill.setBoughtBool(skillBoughtCheckbox.getValue());
             skillList.add(existingSkill);
         }
-
-        existingSkill.setName(skill.getName());
-        existingSkill.setCharacteristic(skill.getCharacteristic());
-        existingSkill.setStartValue(baseField.getValue());
-        existingSkill.setBonusValue(modifierField.getValue());
-        existingSkill.setPenaltyValue(penaltyField.getValue());
-        existingSkill.setBoughtBool(skillBoughtCheckbox.getValue());
-
 
         privateCharacter.setSkills(skillList);
     }
